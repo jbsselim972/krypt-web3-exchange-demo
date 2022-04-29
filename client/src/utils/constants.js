4ee837fd0780b759c922b8a79cbd4e591e78846c
jbsselim <selim.jean-baptiste-simonne@epitech.eu>
2022-04-29 23:19:37 +0200

    fixed a little mistake

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -3,4 +3,4 @@ import abi from "./Transactions.json";
 import.meta.env.VITE_GIPHY_API;
 
 export const contractABI = abi.abi;
-export const contractAddress = VITE_CONTRACT_ADDRESS;
+export const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
